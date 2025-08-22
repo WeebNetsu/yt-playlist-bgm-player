0c96d82777b36053085da5e6993319a283463c43
Stephen <stephenvdw7777@gmail.com>
2025-08-22 18:13:32 +0200

    Init dart project

---
+++ bin/ytbgmplayer.dart
@@ -0,0 +1,5 @@
+import 'package:ytbgmplayer/ytbgmplayer.dart' as ytbgmplayer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ytbgmplayer.calculate()}!');
+}
---
+++ test/ytbgmplayer_test.dart
@@ -0,0 +1,8 @@
+import 'package:ytbgmplayer/ytbgmplayer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
