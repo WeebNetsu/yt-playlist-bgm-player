de44a62bfaaa3dacfa2c4c43f863a331a915d6c7
Stephen <stephenvdw7777@gmail.com>
2025-09-25 12:15:50 +0200

    Add basic dart implementation

--- bin/ytbgmplayer.dart
+++ bin/ytbgmplayer.dart
@@ -1,5 +1,11 @@
-import 'package:ytbgmplayer/ytbgmplayer.dart' as ytbgmplayer;
+// import 'package:ytbgmplayer/ytbgmplayer.dart' as ytbgmplayer;
 
 void main(List<String> arguments) {
-  print('Hello world: ${ytbgmplayer.calculate()}!');
+  //   print('Hello world: ${ytbgmplayer.calculate()}!');
+
+  final menuOptions = ["Play Playlists", "Add Playlist", "Edit Playlist", "Remove Playlist", "Help"];
+
+  var running = true;
+
+  while (running) {}
 }
--- lib/ytbgmplayer.dart
+++ lib/ytbgmplayer.dart
@@ -1,3 +1,36 @@
+import 'package:mpv_dart/mpv_dart.dart';
+
 int calculate() {
   return 6 * 7;
+}
+
+Future<void> testmpv() async {
+  try {
+    MPVPlayer mpvPlayer = MPVPlayer(
+      audioOnly: true,
+      debug: true,
+      verbose: true,
+      mpvArgs: [
+        "--input-ipc-server=/tmp/my_unique_mpv.sock",
+        "--script-opts=ytdl_hook-ytdl_path=/home/netsu/Downloads/yt-dlp",
+      ],
+    );
+    await mpvPlayer.start();
+    await mpvPlayer.load("https://www.youtube.com/watch?v=Fp8msa5uYsc");
+
+    mpvPlayer.on(MPVEvents.started, null, (ev, context) {
+      print("MPV STARTED PLAYING");
+    });
+
+    mpvPlayer.on(MPVEvents.status, null, (ev, context) {
+      print("MPV STATUS CHANGE: ${ev.eventData}");
+    });
+
+    mpvPlayer.on(MPVEvents.timeposition, null, (ev, context) {
+      print("MPV TIMEPOSITION ${ev.eventData}");
+    });
+  } catch (e, stackTrace) {
+    print(e);
+    print(stackTrace);
+  }
 }
